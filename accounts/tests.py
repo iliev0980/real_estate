6ee424b956bee2caf5e8ca8fa9221d0601b311cb
iliev0980 <ivan.iliev.k@gmail.com>
2021-08-13 16:24:58 +0300

    tests for contact app

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,23 +7,32 @@ from copy import deepcopy
 
 
 def create_user(**params):
-    return get_user_model().objects.create_user(**params)
+    payload = {
+        'first_name': 'test first',
+        'last_name': 'test second',
+        'username': 'testuser',
+        'email': 'test@gmail.com',
+        'password': 'test123',
+    }
+    return get_user_model().objects.create_user(**payload)
+
 
 class UserTest(TestCase):
     def setUp(self):
         self.client = Client()
+        create_user()
+
 
     def test_get_register_page(self):
         res = self.client.get(reverse('register'))
         self.assertTemplateUsed(res, 'accounts/register.html')
 
-
-    def test_creating_user(self):
+    def test_creating_user_success_returns_message_and_redirects(self):
         payload = {
-            'first_name': 'test first',
-            'last_name': 'test second',
-            'username': 'testuser',
-            'email': 'test@gmail.com',
+            'first_name': 'first name',
+            'last_name': 'second name',
+            'username': 'testcreateuser',
+            'email': 'testuser@gmail.com',
             'password': 'test123',
             'password2': 'test123',
         }
@@ -32,8 +41,7 @@ class UserTest(TestCase):
         self.assertEqual(str(messages[0]), 'You are now registered and can log in')
         self.assertRedirects(res, reverse('login'))
 
-
-    def test_passwords_doesnt_match_returns_message(self):
+    def test_passwords_doesnt_match_returns_message_and_redirects(self):
         payload = {
             'first_name': 'test first',
             'last_name': 'test second',
@@ -48,10 +56,6 @@ class UserTest(TestCase):
         self.assertEqual(str(messages[0]), 'Passwords do not match!')
         self.assertRedirects(res, reverse('register'))
 
-
-
-
-
     def test_username_already_exists_returns_message_and_redirects(self):
         payload = {
             'first_name': 'test first',
@@ -62,33 +66,54 @@ class UserTest(TestCase):
             'password2': 'test123',
         }
 
-        payload_clean = deepcopy(payload)
-        payload_clean.popitem()
-        create_user(**payload_clean)
         res = self.client.post(reverse('register'), payload)
         messages = list(get_messages(res.wsgi_request))
         self.assertEqual(str(messages[0]), 'That username is taken')
         self.assertRedirects(res, reverse('register'))
 
-
-
-    def test_email_already_exists_returns_message(self):
+    def test_email_already_exists_returns_message_and_redirects(self):
         payload = {
             'first_name': 'test first',
             'last_name': 'test second',
-            'username': 'testuser',
+            'username': 'testuser2',
             'email': 'test@gmail.com',
             'password': 'test123',
             'password2': 'test123',
         }
 
-        payload_clean = deepcopy(payload)
-        payload_clean['username'] = 'testuser2'
-        payload_clean.popitem()
-        create_user(**payload_clean)
         res = self.client.post(reverse('register'), payload)
         messages = list(get_messages(res.wsgi_request))
         self.assertEqual(str(messages[0]), 'This email is beings used')
         self.assertRedirects(res, reverse('register'))
 
+    def test_login_success_returns_message_and_redirects(self):
+        payload = {
+            'username': 'testuser',
+            'password': 'test123'
+        }
 
+        res = self.client.post(reverse('login'), payload)
+        messages = list(get_messages(res.wsgi_request))
+        self.assertEqual(str(messages[0]), 'you are now logged in')
+        self.assertRedirects(res, reverse('dashboard'))
+
+    def test_login_failed_returns_message_and_redirects(self):
+        payload = {
+            'username': 'testuserfail',
+            'password': 'test123'
+        }
+
+        res = self.client.post(reverse('login'), payload)
+        messages = list(get_messages(res.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Invalid credentials')
+        self.assertRedirects(res, reverse('login'))
+
+    def test_logout_returns_message_and_redirects(self):
+        res = self.client.post(reverse('logout'))
+        messages = list(get_messages(res.wsgi_request))
+        self.assertEqual(str(messages[0]), 'You are now logged out')
+        self.assertRedirects(res, reverse('index'))
+
+    def test_dashboard(self):
+        res = self.client.get(reverse('dashboard'))
+        self.assertTemplateUsed(res, 'accounts/dashboard.html')
--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,129 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.contrib.messages import get_messages
+import tempfile
+from listings.models import Listing
+from realtors.models import Realtor
+from .models import Contact
 
-# Create your tests here.
+def create_user(name):
+    payload = {
+        'first_name': 'test first',
+        'last_name': 'test second',
+        'username': f'testuser{name}',
+        'email': f'test{name}@gmail.com',
+        'password': 'test123',
+    }
+    return get_user_model().objects.create_user(**payload)
+
+def create_contact(name):
+    user = create_user(name)
+    listing = create_listing(name)
+    payload = {
+        'listing': listing,
+        'listing_id': listing.id,
+        'name': 'testName',
+        'email': 'testemail@gmail.com',
+        'phone': '123456789',
+        'message': 'test message',
+        'contact_date': '2011-11-11',
+        'user_id': user.id,
+    }
+    return Contact.objects.create(**payload)
+
+
+def create_realtor(name):
+    payload = {
+        'name': f'testRealtor{name}',
+        'photo': tempfile.NamedTemporaryFile(suffix=".jpg").name,
+        'description': 'some description',
+        'phone': '123456789',
+        'email': f'testEmail{name}@gmail.com',
+        'is_mvp': 'False',
+        'hire_date': '2012-12-12'
+    }
+    return Realtor.objects.create(**payload)
+
+
+def create_listing(name):
+    realtor = create_realtor(name)
+    payload = {
+        'realtor': realtor,
+        'title': f'Test Property {name}',
+        'address': f'test adrres',
+        'city': f'testCity',
+        'state': f'testState',
+        'zipcode': '1234',
+        'price': '12345',
+        'bedrooms': '1',
+        'bathrooms': '2',
+        'sqft': '123',
+        'lot_size': '1',
+        'photo_main': tempfile.NamedTemporaryFile(suffix=".jpg").name,
+    }
+    return Listing.objects.create(**payload)
+
+
+class ContactTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.contact = create_contact('setup')
+
+    def test_creating_contact_not_authenticated_user_success(self):
+        listing = create_listing('testListing')
+        payload = {
+            'listing': listing.title,
+            'listing_id': listing.id,
+            'name': 'testName',
+            'email': 'testemail@gmail.com',
+            'phone': '123456789',
+            'message': 'test message',
+            'contact_date': '2011-11-11',
+            'user_id': '0',
+            'realtor_email': listing.realtor.email
+        }
+        res = self.client.post(reverse('contact'), payload)
+        messages = list(get_messages(res.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Your request has benn submitted, a realtor will get back to you soon')
+        self.assertRedirects(res, reverse('listings') + str(listing.id))
+
+
+    def test_creating_contact_authenticated_user_success(self):
+        listing = create_listing('testListing')
+        payload = {
+            'listing': listing.title,
+            'listing_id': listing.id,
+            'name': 'testName',
+            'email': 'testemail@gmail.com',
+            'phone': '123456789',
+            'message': 'test message',
+            'contact_date': '2011-11-11',
+            'user_id': '0',
+            'realtor_email': listing.realtor.email
+        }
+        self.client.login(username='testusersetup', password='test123')
+        res = self.client.post(reverse('contact'), payload)
+        messages = list(get_messages(res.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Your request has benn submitted, a realtor will get back to you soon')
+        self.assertRedirects(res, reverse('listings') + str(listing.id))
+
+    def test_creating_existing_contact(self):
+        payload = {
+            'listing': self.contact.listing.title,
+            'listing_id': self.contact.listing_id,
+            'name': 'testName',
+            'email': 'testemail@gmail.com',
+            'phone': '123456789',
+            'message': 'test message',
+            'contact_date': '2011-11-11',
+            'user_id': self.contact.user_id,
+            'realtor_email': self.contact.listing.realtor.email
+        }
+        self.client.login(username='testusersetup', password='test123')
+        res = self.client.post(reverse('contact'), payload)
+        messages = list(get_messages(res.wsgi_request))
+        self.assertEqual(str(messages[0]), 'You have already made an inquiry for this listing')
+        self.assertRedirects(res, reverse('listings') + str(self.contact.listing_id))
